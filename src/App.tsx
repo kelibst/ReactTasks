36bbde043da7cbcf8f58b81238a6ff740aec326b
kelibst <kbooster17@gmail.com>
2022-12-30 14:48:03 +0000

    added draggable functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import { useState } from 'react'
 import './App.css'
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda} from '@syncfusion/ej2-react-schedule'
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, DragAndDrop,Resize} from '@syncfusion/ej2-react-schedule'
+
+const localData = {
+  dataSource: [
+    {
+    Id:1,
+    StartTime: new Date(2022, 11, 30, 6, 0),
+    EndTime: new Date(2022, 11, 30, 8, 0),
+    Subject: "Blocked",
+    IsBlock: true,
+    
+    },
+    {
+    Id:2,
+    EndTime: new Date(2022, 11, 30, 9, 0),
+    StartTime: new Date(2022, 11, 30, 6, 0),
+    Subject: "Second Testing",
+    
+    IsReadOnly:true,
+    },
+    {
+    Id:3,
+    EndTime: new Date(2022, 11, 30, 10, 0),
+    StartTime: new Date(2022, 11, 30, 6, 0),
+    subject: "Testing",
+    
+    },
+    {
+    Id:4,
+    EndTime: new Date(2022, 11, 30, 12, 0),
+    StartTime: new Date(2022, 11, 30, 6, 0),
+    Subject: "Testing",
+    }
+  ],
+}
+console.log(localData)
 
 function App() {
 
   return (
     <div className="App">
-      <ScheduleComponent>
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      <ScheduleComponent
+      eventSettings={localData}>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize]} />
       </ScheduleComponent>
     </div>
   )
